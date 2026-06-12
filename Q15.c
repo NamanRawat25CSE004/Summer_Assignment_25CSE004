f1b06371bdc79f9a481f1321100907d2dc013ba6
NamanRawat25CSE004 <naman.rawat2025@glbajajgroup.org>
2026-06-12 08:10:21 +0530

    Add files via upload

---
+++ Q15.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int num, temp, digit;
+    int sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    temp = num;
+
+    while(temp > 0)
+    {
+        digit = temp % 10;
+        sum = sum + (digit * digit * digit);
+        temp = temp / 10;
+    }
+
+    if(sum == num)
+        printf("Armstrong Number");
+    else
+        printf("Not an Armstrong Number");
+
+    return 0;
+}
