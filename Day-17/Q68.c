3fdffa7f54b65a83fd36e5c24c3399d23fcffbe3
NamanRawat25CSE004 <naman.rawat2025@glbajajgroup.org>
2026-06-25 11:24:08 +0530

    Add files via upload

---
+++ Day-17/Q68.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main() {
+    int arr1[100], arr2[100];
+    int n1, n2, i, j, found;
+
+    printf("Enter size of first array: ");
+    scanf("%d", &n1);
+
+    printf("Enter elements:\n");
+    for(i = 0; i < n1; i++) {
+        scanf("%d", &arr1[i]);
+    }
+
+    printf("Enter size of second array: ");
+    scanf("%d", &n2);
+
+    printf("Enter elements:\n");
+    for(i = 0; i < n2; i++) {
+        scanf("%d", &arr2[i]);
+    }
+
+    printf("Common elements are:\n");
+
+    for(i = 0; i < n1; i++) {
+
+        found = 0;
+
+        for(j = 0; j < n2; j++) {
+
+            if(arr1[i] == arr2[j]) {
+                found = 1;
+                break;
+            }
+
+        }
+
+        if(found == 1) {
+            printf("%d ", arr1[i]);
+        }
+    }
+
+    return 0;
+}
